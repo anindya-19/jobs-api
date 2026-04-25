2db6780887552f71367c902273cf58ac3930ba98
Anindya Ganguly <anindyaganguly19@gmail.com>
2026-04-25 13:02:46 +0530

    feat: implement user registration, authentication model, and global error handling middleware

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcryptjs");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -19,9 +19,13 @@ const UserSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, "Please provide a valid password"],
-    minglength: 6,
-    maxlength: 12,
+    minlength: 6,
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt); //the keyword this refers to the Document that is currently being saved.
 });
 
 module.exports = mongoose.model("user", UserSchema);
